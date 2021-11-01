4b2b7f814f0daf346e070c255502123b6e818bf8
Francis Dolan <francis.dolan@gmail.com>
2021-11-01 20:42:41 +0000

    Removed commented out code

--- src/Card.js
+++ src/Card.js
@@ -2,24 +2,6 @@ export default function Card(props) {
     let classname = "card " + props.title.toLowerCase().replace(" ", "-");
 
     return (
-        // <div className={classname}>
-        //     <div className="card__top"></div>
-        //     <div className="card__bottom">
-        //         <div className="card__bottom__heading">
-        //             <div className="card__bottom__heading__title">{ props.title }</div>
-        //             <div className="card__bottom__heading__settings"></div>
-        //         </div>
-        //         <div className="card__bottom__time">
-        //             <div className="card__bottom__time__current">
-        //                 { props.timeframe.current }hrs
-        //             </div>
-        //             <div className="card__bottom__time__previous">
-        //                 Last Week - { props.timeframe.previous }hrs
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
-
         <div className={classname}>
             <div className="card__body">
                 <div className="card__body__heading">
