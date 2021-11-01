816bc3956e05264a2c98562d217154398fd1845b
Francis Dolan <francis.dolan@gmail.com>
2021-11-01 20:41:46 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './sass/main.scss';
+import data from './data.json';
+import Card from './Card.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: data,
+    }
+  }
+
+  render() {
+    let cards = data.map(d => (
+      <Card key={d.title} title={d.title} timeframe={d.timeframes.daily}/>
+    ))
+
+    return (
+      <div className="container">
+        { cards }
+      </div>
+    )
+  }
 }
 
 export default App;
